e6c52f4d254001d64e6415da5800d248e9641293
Jim Brown <jim.brown@parivedasolutions.com>
2020-01-12 22:00:30 -0600

    implemented firebase utils for auth and db

--- src/components/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/components/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import './sign-in-and-sign-up.styles.scss';
 import SignIn from '../sign-in/sign-in.component';
+import SignUp from '../sign-up/sign-up.component';
 
 const SignInAndSignUpPage = () => (
     <div className='sign-in-and-sign-up'>
-        <div>
             <SignIn />
-        </div>
+            <SignUp />
     </div>
 );
 
